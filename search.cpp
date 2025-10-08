319a4f33f3f566a55b56d2b3e7351fa1ba615893
smyamajala <msyamajala@ucdavis.edu>
2025-10-08 16:17:08 -0700

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -6,7 +6,9 @@
 // the searches work differently with different runtimes, but the user gets to chose... 
 // poor thing...
 
-
+// LINEAR SEARCH
+// the main essence of this kind of search is to just sift through the list until
+// it finds the target
 
 bool linearSearch(const std::vector<Book>& books, const Book& target) {
     for (const Book& book : books) {
@@ -17,7 +19,12 @@ bool linearSearch(const std::vector<Book>& books, const Book& target) {
     return false;
 }
 
-// 
+// ITERATIVE BINARY SEARCH
+// this search is for sorted data
+// if you recall from book.cpp and book.hpp we did sort the books using 
+// using operation overloading... 
+// the main essence of this search is to continue splitting the search range 
+// in half and comparing both sides until we find the target 
 
 bool binarySearch(const std::vector<Book>& books, const Book& target) {
     int left = 0;
@@ -39,6 +46,11 @@ bool binarySearch(const std::vector<Book>& books, const Book& target) {
     return false;
 }
 
+// RECURSIVE BINARY SEARCH (helper)
+// does the actual messy work
+// the parameters for the actual recursive search is just the vector and the target... 
+// HOWEVER we also need the left and right sides as parameters... this will account for that
+
 bool recursiveBinarySearchHelper(const std::vector<Book>& books, 
                                 const Book& target, 
                                 int left, int right) {
@@ -58,6 +70,8 @@ bool recursiveBinarySearchHelper(const std::vector<Book>& books,
         return recursiveBinarySearchHelper(books, target, left, mid - 1);
     }
 }
+
+// calls the helper to account for the messy parameters...
 
 bool recursiveBinarySearch(const std::vector<Book>& books, const Book& target) {
     return recursiveBinarySearchHelper(books, target, 0, books.size() - 1);

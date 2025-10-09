9777514e2d17811c30adf6af0e4a1deeb0d03b3d
smyamajala <msyamajala@ucdavis.edu>
2025-10-09 09:42:04 -0700

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,46 +1,36 @@
 // Include the Timer class definition from our header file
 #include "timer.hpp"
 
-/**
- * TIMER CONSTRUCTOR
- * 
- * Creates a Timer object and immediately starts timing
- * When you create a Timer, it automatically begins counting
- * from the moment of creation
- */
+// CONSTRUCTOR
+// think of this as calling back to the timer "class" we created in the .hpp file
+
 Timer::Timer() {
     Reset();  // Start the timer as soon as the object is created
 }
 
-/**
- * RESET METHOD
- * 
- * Restarts the timer by setting the start_time to the current moment
- * Use this when you want to begin timing a new operation
- * This is like pressing the "reset" button on a stopwatch
- */
+// RESET FUNCTION
+// we are defining WHAT the reset function will do now
 void Timer::Reset() {
     // Capture the current time from the high-resolution clock
     // This becomes our new starting point for timing measurements
-    start_time = std::chrono::high_resolution_clock::now();
+    start_time = std::chrono::high_resolution_clock::now(); // straight from assignment 
 }
 
-/**
- * CURRENTTIME METHOD
- * 
- * Calculates how much time has elapsed since the last Reset()
- * Returns the time in microseconds (millionths of a second)
- * 
- * @return: elapsed time in microseconds as a double precision number
- */
+
+// CURRENT TIME 
+// gets time "elapsed"
+
 double Timer::CurrentTime() const {
     // Capture the current time to compare with our start time
+    
     auto end = std::chrono::high_resolution_clock::now();
     
-    // Calculate the time difference between end and start
-    // std::chrono::duration<double, std::micro> means:
-    // - Measure the duration as a double-precision number
-    // - Convert it to microseconds (millionths of a second)
-    // - .count() extracts the numerical value from the duration object
+    // calculate the time difference between end and start
+    // std::chrono::duration<double, std::micro> will allow us to get the time in milliseconds
+    // just like we wanted
     return std::chrono::duration<double, std::micro>(end - start_time).count();
+
+// all in all you can think of this like a difference problem where we first get the time at the beginning and the end and then
+// find the total amount of change...
+// it's just mixed with fancy wording for some reason
 }
